b715acd69f72aabf4ffcdde354e51ca9b1418ad3
Santiago <Martnez>
2021-07-16 14:53:44 +0200

    Añadido control de errores sobre la app.js, pequeñas modificaciones en HTML y CSS en cuanto a estilos y refactorización de código. Queda añadir más mapas y comentar código

--- js/app.js
+++ js/app.js
@@ -5,8 +5,18 @@ let tablero = [
 ];
 
 const app = {
+  intentos: 0,
+  hundido: 0,
+  barcos: 0,
+  numBarcos: document.getElementById("numBarcos"),
+  ganador: document.getElementById("ganador"),
+  divTablero: document.getElementById("tablero"),
+  info: document.getElementById("info"),
+  error: document.getElementById("error"),
+
   cargarTablero: function () {
-    this.divTablero = document.getElementById("tablero");
+    numBarcos.innerHTML = `Hay ${this.barcos} barcos`;
+    ganador.innerHTML = `Llevas ${this.intentos} intentos`;
 
     if (this.divTablero.childNodes.length > 0) {
       this.divTablero.innerHTML = " ";
@@ -30,12 +40,12 @@ const app = {
         }
       }
     }
+    this.comprobarVictoria();
   },
 
   iniciar: function () {
-    const titulo = document.getElementById("titulo");
     const abecedario = "abcdefghijklmnopqrstuvwxyz";
-    titulo.innerHTML = "Introduce coordenadas y clica!";
+    info.innerHTML = "Introduce coordenadas y clica!";
 
     document.getElementById("accion").addEventListener(
       "click",
@@ -54,21 +64,53 @@ const app = {
   },
 
   comprobacion: function (letra, numero) {
-    if (tablero[letra][numero] === 1) {
-      tablero[letra][numero] = "x";
-      this.crearEspacios();
-      this.cargarTablero();
-    } else if (tablero[letra][numero] === 0) {
-      tablero[letra][numero] = "o";
-      this.crearEspacios();
-
-      this.cargarTablero();
+    error.innerHTML = " ";
+    try {
+      if (tablero[letra][numero] === 1) {
+        tablero[letra][numero] = "x";
+        this.crearEspacios();
+        this.hundido++;
+        this.intentos++;
+        this.cargarTablero();
+      } else if (tablero[letra][numero] === 0) {
+        tablero[letra][numero] = "o";
+        this.crearEspacios();
+        this.intentos++;
+        this.cargarTablero();
+      } else {
+        this.mensajeError();
+      }
+    } catch (e) {
+      console.log(e);
+      this.mensajeError();
     }
   },
+
+  mensajeError: function () {
+    error.innerHTML = `Te has equivocado de coordenadas!`;
+  },
+
   crearEspacios: function () {
     let br = document.createElement("br");
     this.divTablero.appendChild(br);
   },
-};
 
+  numeroDeBarcos: function () {
+    for (let i = 0; i < tablero.length; i++) {
+      for (let j = 0; j < tablero[i].length; j++) {
+        if (tablero[i][j] === 1) {
+          this.barcos++;
+        }
+      }
+    }
+  },
+  
+  comprobarVictoria: function () {
+    if (this.barcos === this.hundido) {
+      ganador.innerHTML = `Has ganado con ${this.intentos} intentos.<br>
+      Refresca la página para volver a jugar!`;
+    }
+  },
+};
+app.numeroDeBarcos();
 app.iniciar();
